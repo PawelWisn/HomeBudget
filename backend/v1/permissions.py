c3f0e45a5c930a4dfc2905340407182263d24aa7
Pawel Wisniewski <wispawelwis38@gmail.com>
2021-10-12 00:08:53 +0200

    created LoggedIN permission

--- backend/v1/permissions.py
+++ backend/v1/permissions.py
@@ -4,7 +4,15 @@ from . import models
 
 class ActionBasedPermission(permissions.AllowAny):
     def has_permission(self, request, view):
+        print(request.user.is_authenticated, request.user, flush=True)
         for klass, actions in getattr(view, 'action_permissions', {}).items():
             if view.action in actions:
                 return klass().has_permission(request, view)
         return False
+
+class LoggedIn(permissions.BasePermission):
+    message = "Not allowed."
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+

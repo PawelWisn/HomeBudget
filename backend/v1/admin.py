2a94e459af050cebe7e5b252d7176360a8c3f549
Pawel Wisniewski <wispawelwis38@gmail.com>
2021-10-11 22:39:25 +0200

    models are visible in admin panel

--- backend/v1/admin.py
+++ backend/v1/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class BudgetAdmin(admin.ModelAdmin):
+    filter_horizontal = ('participants',)
+    list_display = ('title', 'owner', 'total',)
+    filter_display = ('owner',)
+
+class EntryAdmin(admin.ModelAdmin):
+    list_filter = ('category', 'creator',)
+    list_display = ('budget', 'amount', 'category',)
+
+admin.site.register(Category)
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Budget, BudgetAdmin)

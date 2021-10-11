8a9ef896ae7200b2e4f183654b87838cd3cf89f6
Pawel Wisniewski <wispawelwis38@gmail.com>
2021-10-11 22:40:31 +0200

    superuser is created on project start

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 rm -rf v1/migrations/*
+python manage.py makemigrations v1
 python manage.py makemigrations
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@yahoo.com', 'adminadmin');" | python manage.py shell
 
 exec "$@"

1f961734cfefb6351439071b1a4c909727560eb0
Pawel Wisniewski <wispawelwis38@gmail.com>
2021-10-11 23:11:04 +0200

    created views

--- backend/v1/views.py
+++ backend/v1/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from . import serializers
+from . import models
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    queryset = models.Entry.objects.all()
+    serializer_class = serializers.EntrySerializer
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = models.Budget.objects.all()
+    serializer_class = serializers.BudgetSerializer

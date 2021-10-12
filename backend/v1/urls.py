b55cbebe22082c342ad0d295ebe6fa635e9d41b9
Pawel Wisniewski <wispawelwis38@gmail.com>
2021-10-12 17:49:09 +0200

    added category url

--- backend/v1/urls.py
+++ backend/v1/urls.py
@@ -7,6 +7,7 @@ router = routers.DefaultRouter()
 
 router.register(r'budget', views.BudgetViewSet)
 router.register(r'entry', views.EntryViewSet)
+router.register(r'category', views.CategoryViewSet)
 
 urlpatterns = [
     url('', include(router.urls)),
